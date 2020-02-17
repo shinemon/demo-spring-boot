2a6a1319fa411df74ebea80bdce52d9046e5a7ad
mailshine <mailshine@gmail.com>
2020-02-17 19:04:39 +0000

    Additional changes

--- src/main/java/net/mailshine/demo/demospringboot/controllers/SimpleController.java
+++ src/main/java/net/mailshine/demo/demospringboot/controllers/SimpleController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @RestController
@@ -18,6 +19,13 @@ public class SimpleController {
 
     @GetMapping (value = "/get/employee/list")
     public Set<Employee> employeeSet(){
-      return null;
+        Set <Employee> employees = new HashSet<>();
+        Employee employee = new Employee();
+        employee.setEmployeeId("1");
+        employee.setEmployeeFirstName("John");
+        employee.setEmployeeLastName("Smith");
+        employee.setEmployeeFullName("John Smith");
+        employees.add(employee);
+      return employees;
     }
 }
