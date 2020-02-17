5625f55fe43376d7f61a5a9dac186c9dae9e0690
mailshine <mailshine@gmail.com>
2020-02-17 18:26:42 +0000

    initial commit

---
+++ src/test/java/net/mailshine/demo/demospringboot/DemoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package net.mailshine.demo.demospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
