cd652e552bb7f6ced562a723ff4ffe2f10183cb7
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-02-27 23:27:45 +0000

    changes and junit start

--- spring-boot-microservice/src/main/java/com/mailshine/springboot/microservice/demospringboot/controllers/SimpleController.java
+++ spring-boot-microservice/src/main/java/com/mailshine/springboot/microservice/demospringboot/controllers/SimpleController.java
@@ -10,17 +10,30 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * The type Simple controller.
+ */
 @RestController
 @RequestMapping("/api")
 public class SimpleController {
 
     private static final Logger logger = LoggerFactory.getLogger(SimpleController.class);
 
+    /**
+     * Say hello string.
+     *
+     * @return the string
+     */
     @GetMapping (value = "/get/hello/message")
     public String sayHello(){
         return "Hello There!";
     }
 
+    /**
+     * Employee set set.
+     *
+     * @return the set
+     */
     @GetMapping (value = "/get/employee/list")
     public Set<Employee> employeeSet(){
         Set <Employee> employees = new HashSet<>();
--- spring-boot-microservice/src/main/java/com/mailshine/springboot/microservice/demospringboot/models/Employee.java
+++ spring-boot-microservice/src/main/java/com/mailshine/springboot/microservice/demospringboot/models/Employee.java
@@ -7,6 +7,9 @@ import lombok.ToString;
 
 import java.io.Serializable;
 
+/**
+ * The type Employee.
+ */
 @Getter @Setter @NoArgsConstructor @ToString
 public class Employee implements Serializable {
     private static final long serialVersionUID = 300002228479017363L;
