1e91d1b6d9dfc4a6bf7c68900fcf523cfa66cdf6
mailshine <mailshine@gmail.com>
2020-04-07 01:33:29 +0100

    Spring boot kafka avro serialization and deserialization  example

---
+++ spring-boot-kafka-avro/src/main/java/com/mailshine/springboot/kafka/avro/DemoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.mailshine.springboot.kafka.avro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootApplication.class, args);
+	}
+
+}
