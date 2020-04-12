3c933fbfd59ff1c7f5129a95b3f89d87ca55e1d3
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-04-13 00:02:50 +0100

    active mq additional changes

--- spring-boot-jms-activemq/src/main/java/com/mailshine/demo/jms/activemq/controller/MQController.java
+++ spring-boot-jms-activemq/src/main/java/com/mailshine/demo/jms/activemq/controller/MQController.java
@@ -1,4 +1,13 @@
 package com.mailshine.demo.jms.activemq.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Slf4j
 public class MQController {
+
+    public void postMessage(String message) {
+
+    }
 }
