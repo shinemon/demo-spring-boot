797b274dd9978cfe3eea1233b6c4178127dc4e41
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-04-13 23:08:22 +0100

    webflux additional changes

--- spring-boot-webflux/src/main/java/com/mailshine/springboot/webflux/controller/WebfluxController.java
+++ spring-boot-webflux/src/main/java/com/mailshine/springboot/webflux/controller/WebfluxController.java
@@ -1,9 +1,13 @@
 package com.mailshine.springboot.webflux.controller;
 
+import com.sun.org.apache.xerces.internal.xs.StringList;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -13,5 +17,21 @@ public class WebfluxController {
     public Flux<String> streamString(){
     //todo
         return null;
+    }
+
+
+    private static List<String> stringList(){
+        List<String> stringList = new ArrayList<>();
+        stringList.add("Demo String One");
+        stringList.add("Demo String Two");
+        stringList.add("Demo String Three");
+        stringList.add("Demo String Four");
+        stringList.add("Demo String Five");
+        stringList.add("Demo String Six");
+        stringList.add("Demo String Seven");
+        stringList.add("Demo String Eight");
+        stringList.add("Demo String Nine");
+        stringList.add("Demo String Ten");
+        return stringList;
     }
 }
