e6865e355c95afcc2dededf355c4ffc999aab21c
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-04-13 22:57:21 +0100

    webflux additional changes

--- spring-boot-webflux/src/main/java/com/mailshine/springboot/webflux/controller/WebfluxController.java
+++ spring-boot-webflux/src/main/java/com/mailshine/springboot/webflux/controller/WebfluxController.java
@@ -1,7 +1,17 @@
 package com.mailshine.springboot.webflux.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
+@RequestMapping("/api")
 public class WebfluxController {
+
+    @GetMapping("/stream/notification")
+    public Flux<String> streamString(){
+    //todo
+        return null;
+    }
 }
