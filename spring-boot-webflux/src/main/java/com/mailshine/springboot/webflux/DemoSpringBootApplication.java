b90c830c33454868d6ae4718af337266f54f7b1c
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-04-13 22:27:00 +0100

    webflux additional changes

---
+++ spring-boot-webflux/src/main/java/com/mailshine/springboot/webflux/DemoSpringBootApplication.java
@@ -0,0 +1,21 @@
+package com.mailshine.springboot.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The type Demo spring boot application.
+ */
+@SpringBootApplication
+public class DemoSpringBootApplication {
+
+	/**
+	 * The entry point of application.
+	 *
+	 * @param args the input arguments
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootApplication.class, args);
+	}
+
+}
---
+++ spring-boot-webflux/src/main/java/com/mailshine/springboot/webflux/controller/WebfluxController.java
@@ -0,0 +1,7 @@
+package com.mailshine.springboot.webflux.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebfluxController {
+}
