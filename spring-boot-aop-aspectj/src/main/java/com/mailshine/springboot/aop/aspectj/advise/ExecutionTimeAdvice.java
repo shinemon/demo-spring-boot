e51bddf93541742e9005f98dbc5a7505d4fc8483
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-04-25 18:48:00 +0100

    spring aop and aspectj

---
+++ spring-boot-aop-aspectj/src/main/java/com/mailshine/springboot/aop/aspectj/advise/ExecutionTimeAdvice.java
@@ -0,0 +1,9 @@
+package com.mailshine.springboot.aop.aspectj.advise;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ExecutionTimeAdvice {
+}
