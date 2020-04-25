6f4310314abec148e89b70d0584a00fadc4f24be
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-04-25 17:42:01 +0100

    spring aop and aspectj

---
+++ spring-boot-aop-aspectj/src/main/java/com/mailshine/springboot/aop/aspectj/SpringBootAspectJApplication.java
@@ -0,0 +1,13 @@
+package com.mailshine.springboot.aop.aspectj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAspectJApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAspectJApplication.class, args);
+	}
+
+}
