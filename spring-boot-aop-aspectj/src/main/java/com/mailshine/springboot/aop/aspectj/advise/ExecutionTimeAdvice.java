9a08e4f161583d00fcfd63022645312552bd9827
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-04-25 19:01:01 +0100

    spring aop and aspectj

--- spring-boot-aop-aspectj/src/main/java/com/mailshine/springboot/aop/aspectj/advise/ExecutionTimeAdvice.java
+++ spring-boot-aop-aspectj/src/main/java/com/mailshine/springboot/aop/aspectj/advise/ExecutionTimeAdvice.java
@@ -1,9 +1,22 @@
 package com.mailshine.springboot.aop.aspectj.advise;
 
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Slf4j
 public class ExecutionTimeAdvice {
+
+    @Around("@annotation(com.mailshine.springboot.aop.aspectj.advise.TrackExecutionTime)")
+    public Object executionTime(ProceedingJoinPoint point) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        Object object = point.proceed();
+        long endtime = System.currentTimeMillis();
+        log.info("Method Name: ", point.getSignature() + " time taken for Execution is : " + (endtime-startTime));
+        return object;
+    }
 }
