d357d0766ce8e08122cacd81838b7c7cbb4d0882
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-04-30 23:40:51 +0100

    aspectj n micrometer

---
+++ spring-boot-micrometer/src/main/java/com/mailshine/springboot/micrometer/SpringBootApplication.java
@@ -0,0 +1,20 @@
+package com.mailshine.springboot.micrometer;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * The type Demo spring boot application.
+ */
+@org.springframework.boot.autoconfigure.SpringBootApplication
+public class SpringBootApplication {
+
+	/**
+	 * The entry point of application.
+	 *
+	 * @param args the input arguments
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApplication.class, args);
+	}
+
+}
