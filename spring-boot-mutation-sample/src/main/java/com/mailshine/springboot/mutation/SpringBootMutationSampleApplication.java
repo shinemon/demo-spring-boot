b08ded86da5262e0b443d474bc72fa742073bfdf
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-05-02 15:53:54 +0100

    sample to show case mutation testing

---
+++ spring-boot-mutation-sample/src/main/java/com/mailshine/springboot/mutation/SpringBootMutationSampleApplication.java
@@ -0,0 +1,13 @@
+package com.mailshine.springboot.mutation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMutationSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMutationSampleApplication.class, args);
+	}
+
+}
