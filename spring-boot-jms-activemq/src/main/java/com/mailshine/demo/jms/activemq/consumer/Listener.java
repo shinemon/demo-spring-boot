927e747a393a832a4ec87130c51908a84d5f40c4
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-05-07 23:32:13 +0100

    Spring boot JMS actie mq example

--- spring-boot-jms-activemq/src/main/java/com/mailshine/demo/jms/activemq/consumer/Listener.java
+++ spring-boot-jms-activemq/src/main/java/com/mailshine/demo/jms/activemq/consumer/Listener.java
@@ -1,4 +1,26 @@
 package com.mailshine.demo.jms.activemq.consumer;
 
-public class Listener {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+@Slf4j
+@Component
+public class Listener implements MessageListener {
+
+    @Override
+    public void onMessage(Message message) {
+
+        String payload = null;
+        try {
+            payload = ((TextMessage)message).getText();
+            log.info("Received Message : " +  payload);
+        } catch (JMSException e) {
+            log.error("Error Processing Message " + e.toString());
+        }
+    }
 }
