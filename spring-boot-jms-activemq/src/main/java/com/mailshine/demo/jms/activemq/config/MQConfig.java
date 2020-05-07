02317f7898c7b4c99fc886cf1a6fd0ec5524db2b
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-05-07 23:20:38 +0100

    Spring boot JMS actie mq example

--- spring-boot-jms-activemq/src/main/java/com/mailshine/demo/jms/activemq/config/MQConfig.java
+++ spring-boot-jms-activemq/src/main/java/com/mailshine/demo/jms/activemq/config/MQConfig.java
@@ -1,4 +1,20 @@
 package com.mailshine.demo.jms.activemq.config;
 
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.checkerframework.checker.units.qual.A;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
 public class MQConfig {
+
+    @Bean
+    public ActiveMQConnectionFactory consumerActiveMQConnectionFactory(){
+        ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory();
+        activeMQConnectionFactory.setBrokerURL("");
+        return activeMQConnectionFactory;
+    }
 }
