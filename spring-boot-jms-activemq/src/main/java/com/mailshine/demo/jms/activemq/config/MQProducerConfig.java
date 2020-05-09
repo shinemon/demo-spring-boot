3a8549fe99fbca8165c6d8d3963b4cde2a56e150
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-05-10 00:31:30 +0100

    Spring boot JMS actie mq example

---
+++ spring-boot-jms-activemq/src/main/java/com/mailshine/demo/jms/activemq/config/MQProducerConfig.java
@@ -0,0 +1,9 @@
+package com.mailshine.demo.jms.activemq.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+public class MQProducerConfig {
+}
