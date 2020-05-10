d72db5337dd7adcfcb08acc06e36d3b9009e5979
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-05-11 00:55:35 +0100

    Spring boot JMS actie mq standalone producer n consumer changes

--- spring-boot-standalone-activemq/src/main/java/com/mailshine/springbootstandaloneactivemq/config/ActiveMQConfig.java
+++ spring-boot-standalone-activemq/src/main/java/com/mailshine/springbootstandaloneactivemq/config/ActiveMQConfig.java
@@ -4,9 +4,12 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.core.JmsTemplate;
 
 import javax.jms.ConnectionFactory;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 @Configuration
 public class ActiveMQConfig {
@@ -18,6 +21,7 @@ public class ActiveMQConfig {
     public ConnectionFactory connectionFactory(){
         ActiveMQConnectionFactory activeMQConnectionFactory  = new ActiveMQConnectionFactory();
         activeMQConnectionFactory.setBrokerURL(brokerUrl);
+        activeMQConnectionFactory.setTrustedPackages(Arrays.asList("com.mailshine.springbootstandaloneactivemq"));
         return  activeMQConnectionFactory;
     }
 
@@ -25,7 +29,15 @@ public class ActiveMQConfig {
     public JmsTemplate jmsTemplate(){
         JmsTemplate jmsTemplate = new JmsTemplate();
         jmsTemplate.setConnectionFactory(connectionFactory());
-        jmsTemplate.setPubSubDomain(true);  // enable for Pub Sub to topic
+        jmsTemplate.setPubSubDomain(true);  // enable for Pub Sub to topic. Not Required for Queue.
         return jmsTemplate;
+    }
+
+    @Bean
+    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(){
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory());
+        factory.setPubSubDomain(true);
+        return factory;
     }
 }
--- spring-boot-standalone-activemq/src/main/java/com/mailshine/springbootstandaloneactivemq/jms/JmsConsumer.java
+++ spring-boot-standalone-activemq/src/main/java/com/mailshine/springbootstandaloneactivemq/jms/JmsConsumer.java
@@ -1,10 +1,33 @@
 package com.mailshine.springbootstandaloneactivemq.jms;
 
+import com.mailshine.springbootstandaloneactivemq.model.Employee;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
 
-@Component
-public class JmsConsumer {
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
 
+@Component
+@Slf4j
+public class JmsConsumer implements MessageListener {
+
+
+    @Override
+    @JmsListener(destination = "${active-mq.topic}")
+    public void onMessage(Message message) {
+        try{
+            ObjectMessage objectMessage = (ObjectMessage)message;
+            Employee employee = (Employee)objectMessage.getObject();
+            //do additional processing
+            log.info("Received Message from Topic: "+ employee.toString());
+        } catch(Exception e) {
+            log.error("Received Exception while processing message: "+ e);
+        }
+
+    }
 }
