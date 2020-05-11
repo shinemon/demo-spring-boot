28377f825afaf8dfeeca056a07c3deefc4f92a80
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-05-11 23:48:20 +0100

    Spring boot RabbitMQ sample

---
+++ spring-boot-rabbitmq/src/main/java/com/mailshine/rabbitmq/config/RabbitMQConfig.java
@@ -0,0 +1,9 @@
+package com.mailshine.rabbitmq.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+public class RabbitMQConfig {
+}
