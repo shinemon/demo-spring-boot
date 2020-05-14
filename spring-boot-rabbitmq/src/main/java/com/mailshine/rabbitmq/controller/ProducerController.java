9c3d22ed765b3a85a53d1a0d8796f31178ca6f61
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-05-14 23:48:13 +0100

    Spring boot RabbitMQ sample

---
+++ spring-boot-rabbitmq/src/main/java/com/mailshine/rabbitmq/controller/ProducerController.java
@@ -0,0 +1,9 @@
+package com.mailshine.rabbitmq.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class ProducerController {
+}
---
+++ spring-boot-rabbitmq/src/main/java/com/mailshine/rabbitmq/mq/RabbitMQListener.java
@@ -0,0 +1,4 @@
+package com.mailshine.rabbitmq.mq;
+
+public class RabbitMQListener {
+}
