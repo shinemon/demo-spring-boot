7aa970dcd167cd1fa9de87dd23fd4e2fb5fcf4af
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-05-16 17:37:25 +0100

    Spring boot RabbitMQ config

--- spring-boot-rabbitmq/src/main/java/com/mailshine/rabbitmq/config/RabbitMQConfig.java
+++ spring-boot-rabbitmq/src/main/java/com/mailshine/rabbitmq/config/RabbitMQConfig.java
@@ -1,9 +1,55 @@
 package com.mailshine.rabbitmq.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @Slf4j
 public class RabbitMQConfig {
+
+    @Value("${queue}")
+    private String queueName;
+
+    @Value(("${exchangeName}"))
+    private String exchangeName;
+
+    @Value("${routingKey}")
+    private String routngKey;
+
+    @Bean
+    public Queue queue() {
+    return new Queue(queueName, false);
+    }
+
+    @Bean
+    public DirectExchange exchange(){
+        return new DirectExchange(exchangeName);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, DirectExchange exchange){
+        return BindingBuilder.bind(queue).to(exchange).with(routngKey);
+    }
+
+    @Bean
+    public MessageConverter messageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
+    }
 }
