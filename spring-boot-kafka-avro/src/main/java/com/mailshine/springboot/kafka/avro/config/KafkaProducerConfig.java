d8971209523026d8a056b29f2ce44036ec6414ec
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-05-30 22:32:02 +0100

    spring boot apache avro additional changes in kafka

--- spring-boot-kafka-avro/src/main/java/com/mailshine/springboot/kafka/avro/config/KafkaProducerConfig.java
+++ spring-boot-kafka-avro/src/main/java/com/mailshine/springboot/kafka/avro/config/KafkaProducerConfig.java
@@ -1,5 +1,6 @@
 package com.mailshine.springboot.kafka.avro.config;
 
+import com.mailshine.springboot.kafka.avro.serializer.AvroSerializer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -23,7 +24,7 @@ public class KafkaProducerConfig {
     Map<String, Object> configProps = new HashMap<>();
     configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
     configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-    configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+    configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, AvroSerializer.class);
     return new DefaultKafkaProducerFactory<>(configProps);
   }
 
---
+++ spring-boot-kafka-avro/src/main/java/com/mailshine/springboot/kafka/avro/serializer/AvroSerializer.java
@@ -0,0 +1,57 @@
+package com.mailshine.springboot.kafka.avro.serializer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.avro.generic.GenericDatumWriter;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.io.BinaryEncoder;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.io.EncoderFactory;
+import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.kafka.common.serialization.Serializer;
+
+import javax.xml.bind.DatatypeConverter;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Map;
+
+@Slf4j
+public class AvroSerializer<T extends SpecificRecordBase> implements Serializer<T> {
+
+  @Override
+  public void configure(Map<String, ?> configs, boolean isKey) {
+    // do nothing
+
+  }
+
+  @Override
+  public byte[] serialize(String topic, T payload) {
+    byte[] bytes = null;
+
+    try {
+
+      if (payload != null) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        BinaryEncoder binaryEncoder =
+                EncoderFactory.get().binaryEncoder(byteArrayOutputStream, null);
+
+        DatumWriter<GenericRecord> datumWriter = new GenericDatumWriter<>();
+        datumWriter.write(payload, binaryEncoder);
+
+        binaryEncoder.flush();
+        byteArrayOutputStream.close();
+
+        bytes = byteArrayOutputStream.toByteArray();
+        log.debug("serialized payload='{}'", DatatypeConverter.printHexBinary(bytes));
+      }
+    } catch (IOException e) {
+      log.error("Unable to serialize payload ", e);
+    }
+
+    return bytes;
+  }
+
+  @Override
+  public void close() {
+    // do nothing
+  }
+}
