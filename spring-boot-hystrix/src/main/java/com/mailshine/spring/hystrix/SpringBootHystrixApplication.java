d2c3dff5eb8791eb3d365060905387c3e76c176d
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-06-07 12:09:02 +0100

    spring boot circuit breaker hystrix

---
+++ spring-boot-hystrix/src/main/java/com/mailshine/spring/hystrix/SpringBootHystrixApplication.java
@@ -0,0 +1,13 @@
+package com.mailshine.spring.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHystrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHystrixApplication.class, args);
+    }
+
+}
