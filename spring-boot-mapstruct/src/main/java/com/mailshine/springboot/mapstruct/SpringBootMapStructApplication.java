e9059879eed7374726bae73c42d7153a7f4144ba
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-06-21 18:27:27 +0100

    springboot mapstruct examples

---
+++ spring-boot-mapstruct/src/main/java/com/mailshine/springboot/mapstruct/SpringBootMapStructApplication.java
@@ -0,0 +1,14 @@
+package com.mailshine.springboot.mapstruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringBootMapStructApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMapStructApplication.class, args);
+    }
+
+}
