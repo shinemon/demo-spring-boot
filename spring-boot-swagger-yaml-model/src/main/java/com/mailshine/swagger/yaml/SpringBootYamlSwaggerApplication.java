bbc8c717487ab7189404539c1d1f58489e9b17bd
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-06-28 22:31:31 +0100

    swagger yaml model generate

---
+++ spring-boot-swagger-yaml-model/src/main/java/com/mailshine/swagger/yaml/SpringBootYamlSwaggerApplication.java
@@ -0,0 +1,14 @@
+package com.mailshine.swagger.yaml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringBootYamlSwaggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootYamlSwaggerApplication.class, args);
+    }
+
+}
