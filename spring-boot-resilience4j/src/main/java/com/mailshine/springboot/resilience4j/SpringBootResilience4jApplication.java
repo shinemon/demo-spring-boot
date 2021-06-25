8896cf485f698191864a5d23c2630cae4c4755fa
mailshine <mailshine@gmail.com>
2021-06-25 12:29:31 -0400

    spring boot resilience4j example set up

---
+++ spring-boot-resilience4j/src/main/java/com/mailshine/springboot/resilience4j/SpringBootResilience4jApplication.java
@@ -0,0 +1,11 @@
+package com.mailshine.springboot.resilience4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootResilience4jApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootResilience4jApplication.class, args);
+    }
+}
