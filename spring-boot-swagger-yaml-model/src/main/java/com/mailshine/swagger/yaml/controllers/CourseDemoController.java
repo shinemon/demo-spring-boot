45139bccb2d0f723e4c52487d80379f2d2753262
mailshine <mailshine@gmail.com>
2021-09-02 10:59:16 -0400

    fix for swagger yaml to pojo module

--- spring-boot-swagger-yaml-model/src/main/java/com/mailshine/swagger/yaml/controllers/CourseDemoController.java
+++ spring-boot-swagger-yaml-model/src/main/java/com/mailshine/swagger/yaml/controllers/CourseDemoController.java
@@ -3,9 +3,12 @@ package com.mailshine.swagger.yaml.controllers;
 import com.mailshine.swagger.yaml.model.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
+
 public class CourseDemoController {
 
   @Autowired
